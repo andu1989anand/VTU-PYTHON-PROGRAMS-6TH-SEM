94e9e9bcff411a25d10886bf2e60811bd832d962
ananda kumar hn <andu1989anand@gmail.com>
2018-04-17 12:35:40 +0530

    program to convert temperature from celsius to fahrenheit

---
+++ temperature.py
@@ -0,0 +1,9 @@
+"""program to convert temperature from celsius to fahrenheit"""
+try:
+    temp=input("enter temperature in celsius:")
+    tcel=float(temp)
+    tfahr=tcel*1.8+32
+    print "temperature in Fahrenheit is:",tfahr
+except:
+    print "enter a valid temperature"
+    
